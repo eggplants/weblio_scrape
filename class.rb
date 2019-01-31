c02ac512f127eed6c71020cfe8ad73cfadcc68f6
magipoka <42153744+eggplants@users.noreply.github.com>
2019-02-01 06:23:09 +0900

    Update class.rb

--- class.rb
+++ class.rb
@@ -1,44 +1,87 @@
-#encoding: UTF-8
-require_relative "class"
-begin
+#this program is required "ffplay"
+#
+##to make treatable: CSV wordlist, sentence->noun, level average by each kind
+#
+#...i hate test
+require "uri";require "open-uri"
+class Weblio
 
-  exe="word." +"rd"#debug method
+  def initialize(word)
+    @word = word
+  end
 
-  unless (word = Weblio.new(ARGV[0]))==nil
-      p eval(exe)
-      exit
-    else
-      puts "*plz type something*"
+  #internal
+  def retrieve_data
+    return open(URI.encode("https://ejje.weblio.jp/content/#{@word}")).read
+  end
+  alias_method :rd, :retrieve_data
 
-      loop do
-        unless (word=gets.chomp.to_s)==nil
-          puts "*Well, let me see...*"
 
-          begin
-            ############
-            p eval(exe)
-            ############
-          rescue NoMethodError
-            puts '*none: (…の)いずれも…ない,どれも…ない,少しも…ない,…はよせ,やめなさい,だれも…ない*'
-          rescue OpenSSL::SSL::SSLError || Errno::ECONNRESET
-            puts "*unconnected: つながっていない,関係のない,縁故のない*"
-          rescue SocketError
-            puts "*unreachable: 近づきがたい位置にあるか、状況にある*"
-          end#begin
+  def ret_data_lv
+    return rd.scan(/learning-level-table-wrap(.*)語彙力テストを受ける/).flatten[0]
+    .scan(/>(.*?)</).flatten.delete_if{|a|a==""}
+    .map{|a|a.encode("UTF-8")}.join("/")
+    .scan(/(.*?\/：\/.*?\/|.*?\/：\/.*?$)/)
+  end
+  alias_method :rdl, :ret_data_lv
 
-        else
-          puts "*駄目: tabu,taboo*"
-          next
-        end#unless-2
+  #look up meanings
+  def lookup_s
+    data=rd.mean.read.scan(/content=\"(.*)\"?>/).flatten
+    return data[-1].include?(":") ? data[-1].gsub('"',"") : "*nope: いや,いいえ*"
+  end
+  alias_method :lu_s, :lookup_s
 
-        if word == "exit" ||word == "end" ||word == "finish";sleep(1);puts ".";exit;end
-        puts "*plz type something*"
 
-      end#loop
+  def lookup_a
+    return lookup_s.gsub(/: /,",").split(",")
+  end
+  alias_method :lu_a, :lookup_a
 
-  end#unless-1
 
-#ctrl-C
-rescue Interrupt
-  puts '*interrupt: (…を)さえぎる,中断する,腰を折る*'
+  def lookup_h
+    return {:"#{(v = lu_a).shift}" => v}
+  end
+  alias_method :lu_h, :lookup_h
+
+  #level
+  def level_s
+    return rdl.join(", ").gsub(/&amp;|\//,"").gsub(/：/,":")
+  end
+  alias_method :lv_s, :level_s
+
+
+  def level_a
+    exist = {"レベル":nil,"英検":nil,"学校レベル":nil,"TOEIC® LRスコア":nil}
+    lv_s.split(", ").map{|a|a.split(":")}.map{|o,e|exist["#{o}"]=e}
+    return exist.to_a
+  end
+  alias_method :lv_a, :level_a
+
+
+  def level_h
+    return lv_a.to_h
+  end
+  alias_method :lv_h, :level_h
+
+  #mp3 file of noun
+  def retrieve_mp3
+    return rd.scan(/https:\/\/.*?\.mp3/)[0]
+  end
+  alias_method :r3, :retrieve_mp3
+
+
+  def play_mp3
+    begin
+      if (data=retrieve_mp3) =~ /https?:\/\/[\w\/:%#\$&\?\(\)~\.=\+\-]+/
+        `ffplay -nodisp -loglevel quiet -autoexit -t '231' #{data}`
+      else
+        return '*nope: いや,いいえ*(in mp3)'
+      end
+    rescue
+      return '*nope: いや,いいえ*(in mp3)'
+    end
+  end
+  alias_method :p3, :play_mp3
+
 end
