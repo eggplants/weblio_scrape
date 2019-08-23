b112c021dc227149e09af29ba1036eca684222e3
eggplants <w10776e8w@yahoo.co.jp>
2019-08-23 18:50:10 +0900

    リファクタリング(笑)

--- class.rb
+++ class.rb
@@ -1,90 +1,81 @@
-#this program is required "ffplay"
-#
-##to make available: CSV wordlist, sentence->noun, level average by each kind
-#
-#...i hate test
-require "uri";require "open-uri"
+#to make available: CSV wordlist, sentence - > noun, level average by each kind
+require "uri"
+require "open-uri"
 
 module Weblio
 
-  class GetWordData
-  def initialize(word)
-    @word = word
-  end
+class GetWordData
 
-  #internal
-  def retrieve_data
-    return open(URI.encode("https://ejje.weblio.jp/content/#{@word}")).read
-  end
-  alias_method :rd, :retrieve_data
+def initialize(word)
+  @word = word
+  @data = open(URI.encode("https://ejje.weblio.jp/content/#{@word}")).read
+end
 
-
-  def self.ret_data_lv
-    return rd.scan(/learning-level-table-wrap(.*)語彙力テストを受ける/).flatten[0]
-    .scan(/>(.*?)</).flatten.delete_if{|a|a==""}
-    .map{|a|a.encode("UTF-8")}.join("/")
+def getLevelData
+    return @data.scan(/learning-level-table-wrap(.*)語彙力テストを受ける/).flatten[0]
+    .scan(/>(.*?)</).flatten.delete_if {
+        | a | a == ''
+    }
+    .map {
+        | a | a.encode("UTF-8")
+    }.join("/")
     .scan(/(.*?\/：\/.*?\/|.*?\/：\/.*?$)/)
-  end
-  class << self; alias_method :rdl, :ret_data_lv; end
+    .map {
+        | i | i[0].gsub(/\//, '').sub('&amp;', '').split("：")
+    }.to_h
+end
 
-  #look up meanings
-  def self.lookup_s
-    data=rd.mean.read.scan(/content=\"(.*)\"?>/).flatten
-    return data[-1].include?(":") ? data[-1].gsub('"',"") : "*nope: いや,いいえ*"
-  end
-  class << self; alias_method :lu_s, :lookup_s; end
+# look up meanings
+def meaning_arr
+    return (data = @data.scan(/content=\"(.*)\"?>/).flatten)[-1]
+    .include?(":") ?
+    data[-1].gsub('"', '').sub(/[a-z ]+:/, '').split(',') 
+    : nil
+end
 
+def meaning_hash
+    return {
+    "#{@word}" => meaning_arr
+    }
+end
 
-  def self.lookup_a
-    return lookup_s.gsub(/: /,",").split(",")
-  end
-  class << self; alias_method :lu_a, :lookup_a; end
+# level
 
+def level
+    exist = {
+        "レベル" => nil,
+        "英検" => nil,
+        "学校レベル" => nil,
+        "TOEIC® LRスコア" => nil
+    }
+    return getLevelData.each {
+    | k, v | exist[k] = v
+    }
+end
 
-  def self.lookup_h
-    return {:"#{(v = lu_a).shift}" => v}
-  end
-  class << self; alias_method :lu_h, :lookup_h; end
+# mp3 file of noun
+def getAudioUri
+    return @data.scan(/https:\/\/.*?\.mp3/)[0]
+end
 
-  #level
-  def self.level_s
-    return rdl.join(", ").gsub(/&amp;|\//,"").gsub(/：/,":")
-  end
-  class << self; alias_method :lv_s, :level_s; end
+def getAudio(path = ".")
+    filename = @word + "_audio.mp3"
+    File.open(filename, "w") {
+        | f | f.write open(getAudioUri).read
+    }
+end
 
-
-  def self.level_a
-    exist = {"レベル":nil,"英検":nil,"学校レベル":nil,"TOEIC® LRスコア":nil}
-    lv_s.split(", ").map{|a|a.split(":")}.map{|o,e|exist["#{o}"]=e}
-    return exist.to_a
-  end
-  class << self; alias_method :lv_a, :level_a; end
-
-
-  def self.level_h
-    return lv_a.to_h
-  end
-  class << self; alias_method :lv_h, :level_h; end
-
-  #mp3 file of noun
-  def retrieve_mp3
-    return rd.scan(/https:\/\/.*?\.mp3/)[0]
-  end
-  alias_method :r3, :retrieve_mp3
-
-
-  def self.play_mp3
+def play_mp3# ffmpegが必要
     begin
-      if (data=retrieve_mp3) =~ /https?:\/\/[\w\/:%#\$&\?\(\)~\.=\+\-]+/
-        `ffplay -nodisp -loglevel quiet -autoexit -t '231' #{data}`
-      else
-        return '*nope: いや,いいえ*(in mp3)'
-      end
+        if (data = getAudioUri) =~ /https?:\/\/[\w\/:%#\$&\?\(\)~\.=\+\-]+/
+            `ffplay -nodisp -loglevel quiet -autoexit -t '231' #{data}`
+        else
+            return 'cannot play!'
+        end
     rescue
-      return '*nope: いや,いいえ*(in mp3)'
+        return 'Some error happened!'
     end
-  end
-  class << self; alias_method :p3, :play_mp3; end
+end
 
 end
 
--- class_calltest.rb
+++ class_calltest.rb
@@ -1,4 +1,32 @@
 require_relative "./class.rb"
 str="test"
 o=Weblio::GetWordData.new(str)
-o.p3
+#get_data_lv:rdl:語彙レベル,Hash
+#'{"レベル"=>"1", "英検"=>"3級以上の単語",...}'
+
+#p o.rdl
+
+#lookup_a:lu_a:語彙の意味,Array
+#'[" (能力などをためす)試験", "検査", "(ものの)検査",...]'
+
+#p o.lu_a
+
+#lookup_h:lu_h:単語key,意味value,Hash
+#'{"test"=>[" (能力などをためす)試験", "検査",...]}'
+
+#p o.lookup_h
+
+#level:lv:単語のレベル4つをHashに当てはめる。存在しない値はnil
+# {
+#    "レベル"=>"1"
+#    "英検"=>"3級以上の単語", 
+#    "学校レベル"=>"中学以上の水準", 
+#    "TOEIC® LRスコア"=>"220点以上の単語"
+# }
+
+#p o.level
+
+#au:audiouri:単語mp3のデータURI取得:String
+
+p o.getAudio
+
